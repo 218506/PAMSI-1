9a8b85dbe56c1c8046fa4c10b90bf16c832b53c3
Michal Wiadomski <wiadzix11@gmail.com>
2016-04-13 18:58:20 +0200

    Mergesort dziala.

--- 1403/prog/prj/src/BiegaczLista.cpp
+++ 1403/prog/prj/src/BiegaczLista.cpp
@@ -14,113 +14,134 @@ Lista::Remove(0);
  std::mt19937 rng(rd());
  std::uniform_int_distribution<int> uni(0,rozmiar_docelowy);
 
- tablica_dyn=new string[4];//rozmiar_docelowy];
- /*
+ tablica_dyn=new string[rozmiar_docelowy];
+ 
  //wypelnianie tablicy losowymi liczbami
  for(int i=0; i<rozmiar_docelowy;i++)
    {
-     int n =i;//uni(rng);  //rzutowanie na string
+     int n =uni(rng);  //rzutowanie na string
      std::ostringstream ss;
      ss << n;
      string str = ss.str();
      tablica_dyn[i]=str;
    }
+ 
+   /*
+ tablica_dyn[0]="1";
+ tablica_dyn[1]="5";
+ tablica_dyn[2]="8";
+ //tablica_dyn[3]="11";
+ //tablica_dyn[4]="15";
+ tablica_dyn[3]="4";
+ tablica_dyn[4]="6";
+ tablica_dyn[5]="8";
+ tablica_dyn[6]="13";
+ //tablica_dyn[9]="20";
+ 
  */
- tablica_dyn[0]="3";
- tablica_dyn[1]="1";
- tablica_dyn[2]="2";
- //tablica_dyn[3]="4";
-}
-bool BiegaczLista::run()
-{
-  //quicksort(0,Size()-1);
-
   cerr << "przed "<< endl; 
- for(int i=0;i<2;i++) //rozmiar docelowy!!!
+ for(int i=0;i<rozmiar_docelowy;i++) 
    cerr << tablica_dyn[i] << endl;
 
 
- mergesort(0,4);//Size()-1);
+ mergesort(0,rozmiar_docelowy-1);
 
 
  cerr << "po "<< endl; 
-  for(int i=0;i<3;i++) //rozmiar docelowy!!!
+  for(int i=0;i<rozmiar_docelowy;i++)
    cerr << tablica_dyn[i] << endl;
+
+}
+
+
+bool BiegaczLista::run()
+{
+
   return true;
+  
+  //quicksort(0,Size()-1);
 
 }
 
 /************MERGESORT NA TABLICY*************/
 void BiegaczLista::merge(int l,int s, int p)
 {
-  string *tab=new string[p-l];
-  int i=l;
-  int j=s;
-  int k=l; //licznik listy tmp
+  if(l==p)
+    return;
 
-  for(int n=l;n<p;n++)
+  string* tab=new string[p+1];
+  
+  int i=l;
+  int j=s+1;
+  int k=l; //licznik listy tmp
+  cerr <<"p=  " <<p<< endl;
+  cerr <<"i=  " <<i<< endl;
+  cerr <<"j=  " <<j<< endl;
+  cerr <<"k=  " <<k<< endl; 
+  for(int n=l;n<=p;n++) //Kopiuje do nowej tablicy.
     {
       tab[n]=tablica_dyn[n];
-      cerr<<tab[n];
-    }
-    
-  while(1)
+      //cerr<<tab[n];
+    } 
+  while(i <= s && j <= p)
     {
-      cerr <<"Porownuje: " << tab[i] << " z " <<  tab[j] <<endl; 
-      if(i<s && j<p) //jezeli zaden z ineksow nie wypadl poza swoj zakres
+      if(atoi(tab[i].c_str()) < atoi(tab[j].c_str())) 
 	{
-	  if(atoi(tab[i].c_str()) < atoi(tab[j].c_str())) 
-	    {
-	      cerr << tab[i] <<" < " << tab[j] << endl;
-	      cerr << "wpisuje: " << tab[i] << endl;
-	      this->tablica_dyn[k]=tab[i];
-	      i++;
-	    }
-	  else
-	    {
-	      cerr << tab[i] <<" => " << tab[j] << endl;
-	      cerr << "wpisuje: " << tab[j] << endl;
-	      this->tablica_dyn[k]=tab[j];
-	      j++;
-	    }
+	  cerr << tab[i] <<" < " << tab[j] << endl;
+	  cerr << "wpisuje: " << tab[i] << endl;
+	  tablica_dyn[k]=tab[i];
+	  i++;
 	}
-      else if( i==s && j<p )
+      else
 	{
-	  for(j;j<p;j++)
-	    {
-	      cerr << "przepis1" << endl;
-	      this->tablica_dyn[k]=tab[j];
-	      k++;
-	    }
-	  break;
-	}
-      else if( i<s && j==p)
-	{
-	  for(i;i<s;i++)
-	    {
-	      cerr << "przepis2" << endl;
-	      this->tablica_dyn[k]=tab[i];
-	      k++;
-	    }
-	  break;
+	  cerr << tab[i] <<" > " << tab[j] << endl;
+	  cerr << "wpisuje: " << tab[j] << endl;
+	  tablica_dyn[k]=tab[j];
+	  j++;
 	}
       k++;
-      cerr << "STEP "<< endl; 
-      for(int i=0;i<4;i++) //rozmiar docelowy!!!
-	cerr << tablica_dyn[i] << endl;
+    }
+
+  cerr << "i=  " << i << endl;
+  cerr << "j=  " << j << endl;
+
+  if(j == p+1) //jezeli j wyszlo poza tablice a i jeszcze w swojej polowie
+    {
+      for(i;i<=s;i++)
+	{
+	  cerr << tab[i];
+	  this->tablica_dyn[k]=tab[i];
+	  k++;
+	}
+      //delete tab;
+      return;
+    } 
+
+  if(i == s+1) //jezeli s wyszlo poza swoja polowe
+    {
+      for(j;j<p;j++)
+	{
+	  cerr << tab[i];
+	  this->tablica_dyn[k]=tab[j];
+	  k++;
+	}
+      // delete tab;
+      return;
     }
 }
 
 void BiegaczLista::mergesort(int l, int p)
 {
   int sr;
-  if((p-l) % 2==0)
-    sr=(l+p)/2; //jako, ze p to indeks, aby dostac srodkowy indeks trzeba dodac 1
-  //else
-   
-    
-  cerr << "sr= "<< sr << endl;
-  merge(l,sr,p);
+  sr=(l+p)/2;
+  
+  if(l<p)
+     {    
+      cerr << "sr= "<< sr << endl;
+      mergesort(l,sr);
+      mergesort(sr+1,p);
+      merge(l,sr,p);
+    }
 }
 
 //*************END****************//
